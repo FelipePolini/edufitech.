4529527dc736689e37ea86aac99cf222737ef42d
Felipe Polini <polini.felipe007@gmail.com>
2024-08-25 17:51:52 -0300

    atualização

---
+++ frontend/questions.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        question: "Qual é o principal objetivo dos governos ao cobrar impostos?",
+        answers: [
+            {option: "Financiar festas e eventos públicos", correct: false},
+            {option: "Incentivar o turismo local", correct: false},
+            {option: "Coletar recursos para financiar serviços públicos e infraestrutura", correct: true},
+            {option: "Aumentar os salários dos políticos", correct: false},
+            {option: "Reduzir a quantidade de dinheiro em circulação"},
+        ],
+    },
+    {
+        question: "Qual desses tipos de impostos é cobrado diretamente sobre a renda de uma pessoa?",
+        answers: [
+            {option: "Imposto sobre Valor Agregado (IVA)", correct: false},
+            {option: "Imposto sobre a Renda de Pessoa Física (IRPF)", correct: true},
+            {option: "Imposto Predial e Territorial Urbano (IPTU)", correct: false},
+            {option: "Imposto sobre Produtos Industrializados (IPI)", correct: false},
+            {option: "Imposto de Importação (II)", correct: false},
+        ],
+    },
+    {
+        question: "Qual imposto é geralmente utilizado para financiar a previdência social em muitos países?",
+        answers: [
+            {option: "Imposto sobre Grandes Fortunas", correct: false},
+            {option: "Imposto sobre Transmissão Causa Mortis e Doação (ITCMD)", correct: false},
+            {option: "Contribuição para o Financiamento da Seguridade Social (Cofins)", correct: false},
+            {option: "Contribuição Previdenciária", correct: true},
+            {option: "Imposto sobre Propriedade de Veículos Automotores (IPVA)", correct: false},
+        ],
+    },
+    {
+        question: "Qual é o imposto que incide sobre a propriedade de veículos automotores, como carros e motos?",
+        answers: [
+            {option: "Imposto sobre a Propriedade Predial e Territorial Urbana (IPTU)", correct: false},
+            {option: "Imposto sobre a Propriedade de Veículos Automotores (IPVA)", correct: true},
+            {option: "Imposto sobre Importação (II)", correct: false},
+            {option: "Imposto sobre Operações Financeiras (IOF)", correct: false},
+            {option: "Imposto de Renda de Pessoa Jurídica (IRPJ)", correct: false},
+        ],
+    },
+    {
+        question: "Qual é o nome do imposto que é cobrado quando uma pessoa recebe uma herança ou uma doação?",
+        answers: [
+            {option: "Imposto sobre Produtos Industrializados (IPI)", correct: false},
+            {option: "Imposto de Renda Retido na Fonte (IRRF)", correct: false},
+            {option: "Imposto sobre Valor Agregado (IVA)", correct: false},
+            {option: "Contribuição Social sobre o Lucro Líquido (CSLL)", correct: false},
+            {option: "Imposto sobre Transmissão Causa Mortis e Doação (ITCMD)", correct: true},
+        ],
+    },
+];
